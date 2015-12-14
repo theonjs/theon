640dc8670c55394ebdb164e106e894b591a51391
Tomas Aparicio <tomas@aparicio.me>
2015-12-14 10:21:40 +0000

    refactor(example): misin

--- examples/mixin.js
+++ examples/mixin.js
@@ -16,13 +16,12 @@ var users = client
   .basePath('/api')
   .collection('users')
   .basePath('/users')
-  .mixin('get', function (id, cb) {
+  .mixin('get', function (id) {
     // Create a new request which inherits from
     // the current collection scope to make a custom request
-    var req = this.newRequest()
-    req.path('/:id')
-    req.param('id', 1)
-    req.end(cb)
+    this.path('/:id')
+    this.param('id', 1)
+    return this
   })
   // You can also plug in middleware to the mixin scope
   .use(function (req, res, next) {
@@ -40,6 +39,7 @@ var api = users.render()
 
 // Test mixin
 api.users
-  .get('id', function (err, res) {
+  .get('id')
+  .end(function (err, res) {
     console.log('Response:', res.statusCode, res.body)
   })
